70fca64c2496cf1306f805f2d8b1d2235c70ca53
ThomDumont <tdn.1999@gmail.com>
2018-05-18 15:56:45 -0300

    Update Leao.java

--- projeto lab/Leao.java
+++ projeto lab/Leao.java
@@ -8,5 +8,9 @@ public class Leao extends Player{
 	public Leao(int x, int y,int velo){
 		super(x,y,3);
 	}
+	public int tamanho(int x, int y, int[][] mundo){
+            mundo[x][y] = 'L';
+            
+        }
 
 }

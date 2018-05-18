d49375ed3b28286cc41c5fb60286622b01215d6b
ThomDumont <tdn.1999@gmail.com>
2018-05-18 15:55:09 -0300

    Update Cobra.java

--- projeto lab/Cobra.java
+++ projeto lab/Cobra.java
@@ -5,9 +5,13 @@ import java.util.Random;
 
 public class Cobra extends Player{
 
-	
 	public Cobra(int x, int y,int velo){
 		super(x,y,2);
 	}
-
+        public int tamanho(int x, int y,int[][] mundo){
+            mundo[x][y] = 'C';
+            mundo[x][y+1] = 'C';
+            mundo[x][y+2] = 'C';
+            mundo[x][y+3] = 'C';
+    }
 }

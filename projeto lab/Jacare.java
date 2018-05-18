5ba994234838c496b3f5992e1bf7e812c1c2e15b
ThomDumont <tdn.1999@gmail.com>
2018-05-18 20:27:04 -0300

    Update Jacare.java

--- projeto lab/Jacare.java
+++ projeto lab/Jacare.java
@@ -1,22 +1,28 @@
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Random;
 
 public class Jacare extends Player{
 
-	
 	public Jacare(int x, int y,int velo){
 		super(x,y,1);
 	}
-	public int tamanho(int x, int y, int[][] mundo){
+	int[][]  tamanho() {
+		return null;
+		
+		
+	}
+	int tamanho(int x, int y, int[][] mundo){
             mundo[x][y] = 'J';
             mundo[x+1][y] = 'J';
             mundo[x][y+1] = 'J';
             mundo[x+1][y+1] = 'J';
             mundo[x][y+2] = 'J';
             mundo[x+1][y+2] = 'J';
+            return mundo[x][y];
          }
+	public int getX(){
+    	return x;
+    }
+    public int getY(){
+    	return y;
+    }
 }
 
-}

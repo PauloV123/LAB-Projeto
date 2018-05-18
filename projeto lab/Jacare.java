708a935d60d4badfaf70c34279406ebe74b81eaf
ThomDumont <tdn.1999@gmail.com>
2018-05-18 15:55:54 -0300

    Update Jacare.java

--- projeto lab/Jacare.java
+++ projeto lab/Jacare.java
@@ -9,5 +9,14 @@ public class Jacare extends Player{
 	public Jacare(int x, int y,int velo){
 		super(x,y,1);
 	}
+	public int tamanho(int x, int y, int[][] mundo){
+            mundo[x][y] = 'J';
+            mundo[x+1][y] = 'J';
+            mundo[x][y+1] = 'J';
+            mundo[x+1][y+1] = 'J';
+            mundo[x][y+2] = 'J';
+            mundo[x+1][y+2] = 'J';
+         }
+}
 
 }

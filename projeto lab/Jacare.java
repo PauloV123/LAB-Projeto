f55992a7d41defe974dcc5d1eadf74329f76772f
PauloV123 <paulo_victor99@hotmail.com>
2018-05-18 22:32:10 -0300

    Update Jacare.java

--- projeto lab/Jacare.java
+++ projeto lab/Jacare.java
@@ -1,6 +1,10 @@
-
 public class Jacare extends Player{
-
+	public int getX(){
+    	return x;
+    }
+    public int getY(){
+    	return y;
+    }
 	public Jacare(int x, int y,int velo){
 		super(x,y,1);
 	}
@@ -9,20 +13,15 @@ public class Jacare extends Player{
 		
 		
 	}
-	int tamanho(int x, int y, int[][] mundo){
-            mundo[x][y] = 'J';
-            mundo[x+1][y] = 'J';
-            mundo[x][y+1] = 'J';
-            mundo[x+1][y+1] = 'J';
-            mundo[x][y+2] = 'J';
-            mundo[x+1][y+2] = 'J';
-            return mundo[x][y];
-         }
-	public int getX(){
-    	return x;
-    }
-    public int getY(){
-    	return y;
-    }
-}
+	
+	public void draw(char [][] mundo){
+		mundo[x][y] = 'J';
+        mundo[x+1][y] = 'J';
+        mundo[x][y+1] = 'J';
+        mundo[x+1][y+1] = 'J';
+        mundo[x][y+2] = 'J';
+        mundo[x+1][y+2] = 'J';
+	}       
 
+	
+}

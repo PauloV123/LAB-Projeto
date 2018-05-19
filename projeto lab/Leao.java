ea66f345ad49988a78b1ec01c036a86ea473705c
PauloV123 <paulo_victor99@hotmail.com>
2018-05-18 22:33:01 -0300

    Update Leao.java

--- projeto lab/Leao.java
+++ projeto lab/Leao.java
@@ -1,20 +1,21 @@
 
+
 public class Leao extends Player{
-	
-	public Leao(int x, int y,int velo){
-		super(x,y,3);
-	}
-	int[][] tamanho() {
-		return null;
-	}
-	 int tamanho(int x, int y, int[][] mundo){
-            mundo[x][y] = 'L';
-            return mundo[x][y];
-        }
 	public int getX(){
     	return x;
     }
     public int getY(){
     	return y;
     }
+	public Leao(int x, int y,int velo){
+		super(x,y,3);
+	}
+	int[][] tamanho() {
+		return null;
+	}
+	
+	public void draw(char [][] mundo){
+		mundo[x][y] = 'L';
+	}
+	 	 
 }

ceb55540de8eecfb7342e30f30415a0c6a7eae05
PauloV123 <paulo_victor99@hotmail.com>
2018-05-18 22:31:51 -0300

    Update Cobra.java

--- projeto lab/Cobra.java
+++ projeto lab/Cobra.java
@@ -1,24 +1,21 @@
 
 public class Cobra extends Player{
-
+	 public int getX(){
+     	return x;
+     }
+     public int getY(){
+     	return y;
+     }	
+     
 	public Cobra(int x, int y,int velo){
 		super(x,y,2);
 	}
-	int[][] tamanho() {
-		return null;
-	}
-    public int tamanho(int x, int y,int[][] mundo){
-       mundo[x][y] = 'C';
-       mundo[x][y+1] = 'C';
-       mundo[x][y+2] = 'C';
-       mundo[x][y+3] = 'C';
-       return mundo[x][y];      
-    }
-        public int getX(){
-        	return x;
-        }
-        public int getY(){
-        	return y;
-        }	
+
+	public void draw(char [][] mundo){
+		mundo[x][y] = 'C';
+	    mundo[x][y+1] = 'C';
+	    mundo[x][y+2] = 'C';
+	    mundo[x][y+3] = 'C';
+	}       
 
 }

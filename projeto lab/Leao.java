617033069403b0a0ef7ad12f16e3376fcc413973
ThomDumont <tdn.1999@gmail.com>
2018-05-25 12:12:10 -0300

    Update Leao.java

--- projeto lab/Leao.java
+++ projeto lab/Leao.java
@@ -15,26 +15,5 @@ public class Leao extends Player{
 	
 	public void draw(char [][] mundo){
 		mundo[x][y] = 'L';
-	}
-        
-	public void move( int x, int y, int velo){
-        Random random = new Random();
-		int r = random.nextInt(4);
-		if(r==0){
-		  y = (y+velo)%60;
-		}
-		else if(r==1){
-		  y=(y-velo)%60;
-			if(y<0)
-			   y=60-Math.abs(y);
-		}
-		else if(r==2){
-		  x=(x+velo)%30;
-		}
-		else if(r==3){
-		  x=(x-velo)%30;
-			if(x<0)
-			  x=30-Math.abs(x);
-			}
-		}               	 
+	} 	 
 }

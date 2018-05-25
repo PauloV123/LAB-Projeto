245a55bae4aec22ed0635de3ea9d15fdb8792955
ThomDumont <tdn.1999@gmail.com>
2018-05-25 12:11:22 -0300

    Update Jacare.java

--- projeto lab/Jacare.java
+++ projeto lab/Jacare.java
@@ -16,33 +16,11 @@ public class Jacare extends Player{
 	}
 	
 	public void draw(char [][] mundo){
-		mundo[x][y] = 'J';
+	mundo[x][y] = 'J';
         mundo[x+1][y] = 'J';
         mundo[x][y+1] = 'J';
         mundo[x+1][y+1] = 'J';
         mundo[x][y+2] = 'J';
         mundo[x+1][y+2] = 'J';
-	}
-        
-        public void move( int x, int y, int velo){
-        Random random = new Random();
-		int r = random.nextInt(4);
-		if(r==0){
-		  y = (y+velo)%60;
-		}
-		else if(r==1){
-		  y=(y-velo)%60;
-			if(y<0)
-			   y=60-Math.abs(y);
-		}
-		else if(r==2){
-		  x=(x+velo)%30;
-		}
-		else if(r==3){
-		  x=(x-velo)%30;
-			if(x<0)
-			  x=30-Math.abs(x);
-			}
-		}               
-	
+	}               
 }

f11190c7aa98797e04ec7288879026a33d4c4a06
PauloV123 <paulo_victor99@hotmail.com>
2018-05-27 19:09:04 -0300

    Update Jogador.java

--- projeto lab/Jogador.java
+++ projeto lab/Jogador.java
@@ -1,5 +1,30 @@
 package LAB;
 
 public class Jogador {
+	protected int x;
+	protected int y;
 
+	public Jogador(int x, int y){
+	    this.x = x;
+	    this.y = y;
+        
+	  }
+	  
+	  public void setX(int x){
+	     this.x = x;
+		}
+	  public void setY(int y){
+	     this.y = y;
+		}
+			
+	  public int getX(){
+	      return x;
+		}
+	  public int getY(){
+	      return y;
+		}
+	  public void colocaP(char [][] mundo) {
+		  mundo[0][33] = 'P';
+	  }
 }
+
